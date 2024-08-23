ce8147c1a11b4f49624041dfad3cd46dde7cabb7
RudranshGautam <158027721+RudranshGautam@users.noreply.github.com>
2024-08-23 11:56:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
+// Relationships
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+// Sync database
+sequelize
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Test User', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
---
+++ cart.js
@@ -0,0 +1,30 @@
+// models/cart.js
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const Cart = sequelize.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  }
+});
+
+module.exports = Cart;
+
+// models/cart-item.js
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const CartItem = sequelize.define('cartItem', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  quantity: Sequelize.INTEGER
+});
+
+module.exports = CartItem;
